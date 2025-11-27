ef9a6bcc94a010670f304f4e18570f4c492fb952
Basith <basithzan07@gmail.com>
2025-11-28 01:14:30 +0530

    genesis

--- app/layout.js
+++ app/layout.js
@@ -34,6 +34,26 @@ export default function RootLayout({ children }) {
           }}
         />
         {children}
+        <Script
+          id="genesys-chat"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function (g, e, n, es, ys) {
+                g['_genesysJs'] = e;
+                g[e] = g[e] || function () {
+                  (g[e].q = g[e].q || []).push(arguments)
+                };
+                g[e].t = 1 * new Date();
+                g[e].c = es;
+                ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+              })(window, 'Genesys', 'https://apps.mec1.pure.cloud/genesys-bootstrap/genesys.min.js', {
+                environment: 'prod-mec1',
+                deploymentId: '6de673d2-7a76-40cc-a71d-0f57a14b31af'
+              });
+            `,
+          }}
+        />
       </body>
     </html>
   );
